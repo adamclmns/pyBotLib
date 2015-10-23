3e5033c7b64690df471b90ce9071923314cde40c
AdamClmns <adam@AdamClmns.com>
2015-10-23 17:26:50 -0500

    Commenting Sketch

--- arduinoSource/ArduinoPyBot-4-Servo/ArduinoPyBot-4-Servo.ino
+++ arduinoSource/ArduinoPyBot-4-Servo/ArduinoPyBot-4-Servo.ino
@@ -24,32 +24,40 @@ int servo4Pos;
 
 void setup() {  
   //Attach servos to pins
-  servo1.attach(11);  
-  servo2.attach(10);  
-  servo3.attach(9);  
-  servo4.attach(8);  
+  servo1.attach(7);  
+  servo2.attach(6);  
+  servo3.attach(5);  
+  servo4.attach(4);  
 
+  //THis is lowest value for the MeArm robots 4th servo without damaging it. 
   servo4.write(140);
   Serial.begin(9600);
 }
 
-void loop() {
-
-  //Read data from serial - python syntax is serial.write('90,90,90,90e')
-  while (Serial.available() > 0) {
-    servo1Pos = Serial.parseInt();
-    servo2Pos = Serial.parseInt();
-    servo3Pos= Serial.parseInt();
-    servo4Pos = Serial.parseInt();
-
-    if (Serial.read() == 'x') { //Ending character, can't be parsed to Int
+void writeServoPos(){
       servo1.write(servo1Pos);
       servo2.write(servo2Pos);
       servo3.write(servo3Pos);
+      //Preventing servo 4 from opening too far. Can cause damage. Should be changed for your implementation.
       if (servo4Pos < 140){
         servo4Pos = 140;
       }
       servo4.write(servo4Pos);
+}
+
+void loop() {
+
+  //Read data from serial - python syntax is serial.write('254,90,90,90,90,255')
+  while (Serial.available() > 0) {
+    
+      servo1Pos = Serial.parseInt();
+      servo2Pos = Serial.parseInt();
+      servo3Pos= Serial.parseInt();
+      servo4Pos = Serial.parseInt();
+    
+    if (Serial.parseInt() == 255) { //Write Command == 255
+       writeServoPos();
     }
+   
   }
 }
